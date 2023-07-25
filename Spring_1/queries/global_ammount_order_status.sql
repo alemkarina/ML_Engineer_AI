edd197c2e89b542fd82dfa98451ef2e560e52972
alemkarina <51099848+alemkarina@users.noreply.github.com>
2023-07-25 14:36:41 -0300

    Actualización

--- Spring_1/queries/global_ammount_order_status.sql
+++
@@ -1,7 +0,0 @@
--- TODO: This query will return a table with two columns; order_status, and
--- Ammount. The first one will have the different order status classes and the
--- second one the total ammount of each.
-SELECT order_status, 
-    COUNT(DISTINCT(order_id)) AS Ammount 
-    FROM olist_orders 
-GROUP BY order_status;
